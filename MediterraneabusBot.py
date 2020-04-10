d638e309b95ab3ca15006dd831294c82b75ea5e4
Massimiliano Montaleone <theplayergame97@gmail.com>
2020-04-10 20:58:10 +0200

    update telegram api, update internal api

--- MediterraneabusBot.py
+++ MediterraneabusBot.py
@@ -1,222 +1,191 @@
-from bs4 import BeautifulSoup
-from settings import token, client_file, start_msg, hours, period
-from telepot.namedtuple import ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
-from time import sleep
-from random import randint
+import logging
 import json
-import os
 import requests
-import sys
-import telepot
 
-# State for user
-user_state = {}
+from telegram import Location
+import telegram.error as tg_error
+from telegram import (ParseMode, MessageEntity, ChatAction,
+                      ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (CallbackQueryHandler, CommandHandler, Filters,
+                          MessageHandler, Updater, ConversationHandler)
 
-periodo = {}
-partenza = {}
-arrivo = {}
-arr = {}
-
-markup = ReplyKeyboardMarkup(keyboard=hours)
+from settings import token, client_file, start_msg, periodList, url_api
 
 
-def on_chat_message(msg):
-    content_type, chat_type, chat_id = telepot.glance(msg)
-    command_input = msg['text']
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-    # Check user state
-    try:
-        user_state[chat_id] = user_state[chat_id]
-    except:
-        user_state[chat_id] = 0
-
-    # start command
-    if command_input == "/start":
-        if register_user(chat_id):
-            bot.sendMessage(chat_id, start_msg, parse_mode='Markdown')
-            command_input = "/orari"
-
-    # donate command
-    if command_input == '/dona':
-        keyboard = InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(
-                text="Dona", url='https://paypal.me/Fast0n/')],
-        ])
-        bot.sendMessage(chat_id, "Codice sorgente: \n" +
-                        "[MediterraneabusBot](https://github.com/Fast0n/MediterraneabusBot)\n\n" +
-                        "App Android: \n" +
-                        "[Mediterraneabus App](https://play.google.com/store/apps/details?id=com.fast0n.mediterraneabus)\n\n" +
-                        "Instagram: \n" +
-                        "[Mediterraneabus](instagram.com/mediterraneabus)\n\n" +
-                        "Sviluppato da: \n" +
-                        "[Fast0n](https://github.com/Fast0n)\n\n" +
-                        "🍺 Se sei soddisfatto offri una birra allo sviluppatore 🍺", parse_mode='Markdown', reply_markup=keyboard)
-        user_state[chat_id] = 0
-
-    # orari command
-    if command_input == '/orari':
-        msg = "Seleziona il periodo"
-        markup1 = ReplyKeyboardMarkup(keyboard=[["Scolastico", "Non scolastico"]
-                                                ])
-        bot.sendMessage(chat_id, msg, reply_markup=markup1)
-        user_state[chat_id] = 1
-
-    elif user_state[chat_id] == 1:
-        msg = "Seleziona la partenza"
-        bot.sendMessage(chat_id, msg, reply_markup=markup)
-        user_state[chat_id] = 2
-        periodo[chat_id] = period[command_input]
-
-    elif user_state[chat_id] == 2:
-        # check field
-        for i in range(0, len(hours)):
-            for o in range(0, len(hours[i])):
-                if command_input != hours[i][o]:
-                    arr[chat_id] = 1
-                else:
-                    arr[chat_id] = 0
-                    partenza[chat_id] = command_input
-                    msg = "Seleziona l'arrivo"
-                    bot.sendMessage(chat_id, msg, reply_markup=markup)
-                    user_state[chat_id] = 3
-                    return
-
-        # if field is false
-        if arr[chat_id] == 1:
-            bot.sendMessage(
-                chat_id, "Seleziona la partenza dalla tastiera", reply_markup=markup)
-
-    elif user_state[chat_id] == 3:
-        # if the departure is equal on arrival
-        arrivo[chat_id] = command_input
-        if partenza[chat_id] == arrivo[chat_id]:
-            user_state[chat_id] = 2
-            bot.sendMessage(
-                chat_id, "Non puoi usare la stessa destinazione, ricomincia...")
-        else:
-            for i in range(0, len(hours)):
-                for o in range(0, len(hours[i])):
-                    if command_input != hours[i][o]:
-                        arr[chat_id] = 3
-                    else:
-                        arr[chat_id] = 2
-                        arrivo[chat_id] = command_input
-
-                        # take info from url
-                        URL = "https://mediterraneabus-api.herokuapp.com/?periodo=" + \
-                            periodo[chat_id] + "&percorso_linea=" + \
-                            partenza[chat_id] + \
-                            "&percorso_linea1=" + arrivo[chat_id]
-
-                        r = requests.get(URL)
-                        try:
-                            json1 = json.loads(r.text)
-
-                            final_message = "🚏 Fermata *" + \
-                                (partenza[chat_id]).upper() + \
-                                "*\nOrari *solo* feriali da _LUN-SAB_"
-
-                            for z in range(len(json1['linee'])):
-                                final_message += "\n\n" + (json1['linee'][z]['corsa']).replace(
-                                    "Linea", "*Linea") + "*\n🕜 Orari "
-                                for x in range(len(json1['linee'][z]['orari'])):
-                                    final_message += "_" + \
-                                        json1['linee'][z]['orari'][x]['partenza'] + " _ - "
-
-                                    if x + 1 == len(json1['linee'][z]['orari']):
-                                        final_message = final_message[:-3]
-
-                            try:
-                                # send location
-                                response = requests.get(
-                                    'https://maps.googleapis.com/maps/api/geocode/json?address=' + partenza[chat_id].replace("-", " ") + ',+IT')
-                                resp_json_payload = response.json()
-                                a = str(
-                                    resp_json_payload['results'][0]['geometry']['location']).split(",")
-                                latitude = a[0].replace("{'lat': ", "")
-                                longitude = a[1].replace("'lng': ", "").replace(
-                                    " ", "").replace("}", "")
-                                bot.sendLocation(chat_id, latitude, longitude, reply_markup=ReplyKeyboardRemove(
-                                    remove_keyboard=True))
-                            except:
-                                pass
-
-                            bot.sendMessage(
-                                chat_id, final_message + "\n\nDirezione *" + (
-                                    arrivo[chat_id]).upper() + "*", parse_mode='Markdown', reply_markup=ReplyKeyboardRemove(remove_keyboard=True))
-                                                     
-                            random = randint(1, 3);
-                            if random == 1:
-                             bot.sendMessage(
-                                chat_id, "Sei un utente Android?\n\nScarica l'app [Mediterraneabus](https://play.google.com/store/apps/details?id=com.fast0n.mediterraneabus)", parse_mode='Markdown')
-                            elif random == 2:
-                               keyboard = InlineKeyboardMarkup(inline_keyboard=[
-                                [InlineKeyboardButton(
-                                    text="Dona", url='https://paypal.me/Fast0n/')],
-                            ])
-                            bot.sendMessage(chat_id, "Codice sorgente: \n" +
-                                            "[MediterraneabusBot](https://github.com/Fast0n/MediterraneabusBot)\n\n" +
-                                            "Instagram: \n" +
-                                            "[Mediterraneabus](instagram.com/mediterraneabus)\n\n" +
-                                            "Sviluppato da: \n" +
-                                            "[Fast0n](https://github.com/Fast0n)\n\n" +
-                                            "🍺 Se sei soddisfatto offri una birra allo sviluppatore 🍺", parse_mode='Markdown', reply_markup=keyboard)
-                            user_state[chat_id] = 0
-                            
-                            return
-
-                        except:
-                            bot.sendMessage(chat_id, "*Errore Orario! Non trovato*\nSe desideri cercare un nuovo orario clicca su /orari", parse_mode='Markdown',
-                                            reply_markup=ReplyKeyboardRemove(remove_keyboard=True))
-                        return
+DEPARTURE, ARRIVAL, MAKELIST = range(3)
 
 
-def register_user(chat_id):
-    """
-    Register given user
-    """
-    insert = 1
+def markup_test():
+    json_list = json.loads(requests.get(url_api + "?lista").text)
+    arr = json_list["list"]["routes"]
 
-    try:
-        f = open(client_file, "r+")
-
-        for user in f.readlines():
-            if user.replace('\n', '') == str(chat_id):
-                insert = 0
-
-    except IOError:
-        f = open(client_file, "w")
-
-    if insert:
-        f.write(str(chat_id) + '\n')
-
-    f.close()
-
-    return insert
+    arrs = []
+    while len(arr) > 2:
+        pice = arr[:2]
+        arrs.append(pice)
+        arr = arr[1:]
+    arrs.append(arr)
+    return arrs
 
 
-# Main
-print("Avvio MediterraneabusBot")
+def start(update, context):
+    """Send starting message"""
+    update.message.reply_text(start_msg,  parse_mode=ParseMode.MARKDOWN)
 
-# PID file
-pid = str(os.getpid())
-pidfile = "/tmp/mediterraneabusbot.pid"
 
-# Check if PID exist
-if os.path.isfile(pidfile):
-    print("%s already exists, exiting!" % pidfile)
-    sys.exit()
+def period(update, context):
+    reply_keyboard = [['Scolastico', "Non Scolastico"]]
+    update.message.reply_text("Seleziona il periodo",  parse_mode=ParseMode.MARKDOWN,
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
 
-# Create PID file
-f = open(pidfile, 'w')
-f.write(pid)
+    return DEPARTURE
 
-# Start working
-try:
 
-    bot = telepot.Bot(token)
-    bot.message_loop(on_chat_message)
-    while(1):
-        sleep(10)
-finally:
-    os.unlink(pidfile)
+def departure(update, context):
+    context.user_data['period'] = update.message.text
+
+    reply_keyboard = markup_test()
+    update.message.reply_text("Seleziona la partenza",  parse_mode=ParseMode.MARKDOWN,
+                              reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+    return ARRIVAL
+
+
+def arrival(update, context):
+    # check field
+    hours_raw = json.loads(requests.get(url_api + "?lista").text)
+    hours = hours_raw["list"]["routes"]
+    reply_keyboard = markup_test()
+
+    context.user_data['departure'] = update.message.text
+    for i in range(0, len(hours)):
+        if context.user_data['departure'] == hours[i]:
+            update.message.reply_text("Seleziona l'arrivo",  parse_mode=ParseMode.MARKDOWN,
+                                      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+            return MAKELIST
+
+        elif i == len(hours)-1 and context.user_data['departure'] != hours[len(hours)-1]:
+            update.message.reply_text("Seleziona la partenza dalla tastiera",  parse_mode=ParseMode.MARKDOWN,
+                                      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+
+def makelist(update, context):
+    # check field
+    hours_raw = json.loads(requests.get(url_api + "?lista").text)
+    hours = hours_raw["list"]["routes"]
+    reply_keyboard = markup_test()
+
+    context.user_data['arrival'] = update.message.text
+    for i in range(0, len(hours)):
+        if i == len(hours)-1 and context.user_data['arrival'] != hours[len(hours)-1]:
+            update.message.reply_text("Seleziona la partenza dalla tastiera",  parse_mode=ParseMode.MARKDOWN,
+                                      reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True, resize_keyboard=True))
+
+        elif context.user_data['arrival'] == hours[i]:
+            periodo = periodList[context.user_data['period']]
+
+            # take info from url
+            URL = url_api + \
+                "?periodo=" + periodo + "&percorso_linea=" + \
+                context.user_data['departure'] + \
+                "&percorso_linea1=" + \
+                context.user_data['arrival'] + "&sort_by=time"
+
+            r = requests.get(URL)
+            try:
+                json1 = json.loads(r.text)
+                final_message = "🚏 Fermata *" + \
+                    (context.user_data['departure']).upper() + \
+                    "*\nOrari *solo* feriali da _LUN-SAB_"
+                for z in range(len(json1)):
+                    final_message += "\n\n" + (json1[z]['a']).replace(
+                        "Linea", "*Linea") + "*\n🕜 Orario " + json1[z]['b'] + " - " + json1[z]['c']
+
+                # send location
+                URL = "https://github.com/Fast0n/MediterraneabusBot/blob/master/geocode.csv"
+                r = requests.get(URL)
+                for row in range(0, len((r.text.split("\n")))):
+                    a = r.text.split("\n")
+                    b = a[row].split(",")
+                    if context.user_data['departure'] in b[2]:
+                        latitude = b[0]
+                        longitude = b[1]
+
+                context.bot.send_location(
+                    chat_id=update.message.chat_id, latitude=latitude, longitude=longitude)
+                update.message.reply_text(final_message + "\n\nDirezione *" + (
+                    context.user_data['arrival']).upper() + "*",  parse_mode=ParseMode.MARKDOWN,
+                    reply_markup=ReplyKeyboardRemove())
+
+            except Exception as e:
+                print(str(e))
+                update.message.reply_text("*Errore Orario! Non trovato*\nSe desideri cercare un nuovo orario clicca su /orari",  parse_mode=ParseMode.MARKDOWN,
+                                          reply_markup=ReplyKeyboardRemove())
+
+            return ConversationHandler.END
+
+
+def dona(update, context):
+    update.message.reply_text("Codice sorgente: \n" +
+                              "[MediterraneabusBot](https://github.com/Fast0n/MediterraneabusBot)\n\n" +
+                              "Instagram: \n" +
+                              "[Fast0n](instagram.com/Fast0n)\n\n" +
+                              "Sviluppato da: \n" +
+                              "[Fast0n](https://github.com/Fast0n)\n\n" +
+                              "🍺 Se sei soddisfatto offri una birra allo sviluppatore 🍺", parse_mode=ParseMode.MARKDOWN, reply_markup=ReplyKeyboardRemove())
+
+
+def error(update, context):
+    """Log errors caused by updates"""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    """Starts the bot"""
+    print("Avvio MediterraneabusBot")
+
+    # Create the Updater
+    updater = Updater(token, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.text & (~Filters.regex(
+            '^(/start)$') & (~Filters.regex('^(/dona)$'))), period)],
+
+        states={
+
+            DEPARTURE: [MessageHandler(Filters.text & (~Filters.regex('^(/start)$') & (~Filters.regex('^(/dona)$'))), departure)],
+            ARRIVAL: [MessageHandler(Filters.text & (~Filters.regex('^(/start)$') & (~Filters.regex('^(/dona)$'))), arrival)],
+            MAKELIST: [MessageHandler(Filters.text & (~Filters.regex('^(/start)$') & (~Filters.regex('^(/dona)$'))), makelist)],
+
+        },
+
+        fallbacks=[MessageHandler(Filters.text & (
+            Filters.regex('^(Stop)$')), start)]
+
+    )
+
+    # Register handlers
+    dp.add_handler(conv_handler)
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("orari", period))
+    dp.add_handler(CommandHandler("dona", dona))
+
+    # Register error handler
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Mediterraneabus Bot
-<a href="https://paypal.me/fast0n" title="Donate"><img src="https://img.shields.io/badge/Dona-PayPal-009cde.svg?style=flat-square"></a>
+[![Donate](https://img.shields.io/badge/Donate-PayPal-blue?style=flat-square)](https://paypal.me/fast0n) [![License](https://img.shields.io/github/license/Fast0n/MediterraneabusBot)](https://github.com/Fast0n/MediterraneabusBot/blob/master/LICENSE) 
 
 ![Mediterraneabus](img/banner.png)
 
@@ -11,7 +11,7 @@ Ora i passeggeri della Mediterraneabus potranno controllare in tempo reale gli o
 
 ------------------------------------------------------------------------------------
 
-This bot is realized with [Telepot](https://github.com/nickoala/telepot) and [Mediterraneabus-api](https://github.com/fast0n/mediterraneabus-api).
+This bot is realized with [Mediterraneabus-api](https://github.com/fast0n/mediterraneabus-api).
 
 This is an unofficial bot developed to learn. Mediterraneabus S.p.A. is not responsible in any way.
 
